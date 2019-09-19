5ebeebec82c60ab56f5e470218c2c7d1fb128dff
Cuong <cuongdm@toprate.ios>
2019-09-19 21:05:49 +0700

    Update file

---
+++ TestBridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestBridge
+//
+//  Created by ManhCuong on 9/19/19.
+//  Copyright © 2019 encodejsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestBridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBridge
+//
+//  Created by ManhCuong on 9/19/19.
+//  Copyright © 2019 encodejsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
