17372d956ca80b3c59117c2552f9ef50c5ef8a4b
Cuong <cuongdm@toprate.ios>
2019-09-19 21:20:03 +0700

    Add FirstViewController

---
+++ TestBridge/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  TestBridge
+//
+//  Created by ManhCuong on 9/19/19.
+//  Copyright © 2019 encodejsc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
